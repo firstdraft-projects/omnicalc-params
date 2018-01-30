240710bd72c33d9e79d3382d739d3e1b715c665c
Arjun Venkataswamy <avenkat2@gmail.com>
2018-01-30 14:36:27 -0600

    adds hw problem 2

--- spec/features/algorithmia_spec.rb
+++ spec/features/algorithmia_spec.rb
@@ -1,7 +1,7 @@
 require "rails_helper"
 
 describe "/colorize" do
-  it "has a label named 'Image URL'", points: 1, hint: h("copy_must_match label_for_input") do
+  it "has a label named 'Image URL'", points: 1 do
     visit "/colorize"
 
     expect(page).to have_css("label", text: 'Image URL')
@@ -17,7 +17,7 @@ describe "/colorize" do
 end
 
 describe "/colorize" do
-  it "has a button named 'Colorize'", points: 1, hint: h("copy_must_match label_for_input") do
+  it "has a button named 'Colorize'", points: 1 do
     visit "/colorize"
 
     expect(page).to have_css("button", text: 'Colorize')
@@ -25,7 +25,7 @@ describe "/colorize" do
 end
 
 describe "/colorize" do
-  it "correctly displays the image", points: 3 do
+  it "displays the colorized image", points: 5 do
     #====================  Begin setup  ===========================#
     # create fake Algorithmia objects to speed up tests
     Algorithmia = class_double("Algorithmia")
@@ -35,12 +35,61 @@ describe "/colorize" do
     allow(Algorithmia).to receive(:client).and_return(client_obj)
     allow(client_obj).to receive(:algo).and_return(algo_obj)
     allow(algo_obj).to receive(:pipe).and_return(pipe_obj)
-    allow(pipe_obj).to receive(:result).and_return({'output' => 'data://.algo/deeplearning/ColorfulImageColorization/temp/rand-M.png'})
+    allow(pipe_obj).to receive(:result).and_return({'output' => 'data://.algo/deeplearning/ColorfulImageColorization/temp/grantpark-1.0.png'})
     #====================  End setup  =============================#
 
     visit "/colorize"
-    fill_in "Image URL", with: 'image.png'
+    fill_in "Image URL", with: "https://cdn.vox-cdn.com/uploads/chorus_asset/file/4863353/grantpark-1.0.jpg"
     click_button "Colorize"
-    expect(page).to have_css("img[src*='rand-M.png']")
+    expect(page).to have_css("img[src*='.algo/deeplearning/ColorfulImageColorization/temp/grantpark-1.0.png']")
+  end
+end
+
+describe "/autotag" do
+  it "has a label named 'Text'", points: 1, hint: h("copy_must_match label_for_input") do
+    visit "/autotag"
+
+    expect(page).to have_css("label", text: 'Text')
+  end
+end
+
+describe "/autotag" do
+  it "has a textarea", points: 1, hint: h("copy_must_match label_for_input") do
+    visit "/autotag"
+
+    expect(page).to have_css("textarea", count: 1)
+  end
+end
+
+describe "/autotag" do
+  it "has a button named 'Generate Tags'", points: 1, hint: h("copy_must_match label_for_input") do
+    visit "/autotag"
+
+    expect(page).to have_css("button", text: 'Generate Tags')
+  end
+end
+
+describe "/autotag" do
+  it "correctly generates tags", points: 5 do
+    #====================  Begin setup  ===========================#
+    # create fake Algorithmia objects to speed up tests
+    Algorithmia = class_double("Algorithmia2")
+    client_obj = double(:client)
+    algo_obj = double(:algo)
+    pipe_obj = double(:pipe)
+    allow(Algorithmia).to receive(:client).and_return(client_obj)
+    allow(client_obj).to receive(:algo).and_return(algo_obj)
+    allow(algo_obj).to receive(:pipe).and_return(pipe_obj)
+    allow(pipe_obj).to receive(:result).and_return([
+                                                    "beat",
+                                                    "case"
+                                                    ])
+    #====================  End setup  =============================#
+
+    visit "/autotag"
+    fill_in "Text", with: "One of the most common types of advice we give at Y Combinator is to do things that don't scale. A lot of would-be founders believe that startups either take off or don't. You build something, make it available, and if you've made a better mousetrap, people beat a path to your door as promised. Or they don't, in which case the market must not exist."
+    click_button "Generate Tags"
+    expect(page).to have_css("li", text: "beat")
+    expect(page).to have_css("li", text: "case")
   end
 end
